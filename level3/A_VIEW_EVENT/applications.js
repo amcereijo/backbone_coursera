05aead73afd09cba60e02a681db99aaff0c21e39
amcereijo <amcereijo@gmail.com>
2014-07-20 22:58:07 +0200

    level3 assigments

---
+++ level3/A_VIEW_EVENT/applications.js
@@ -0,0 +1,12 @@
+var AppointmentView = Backbone.View.extend({
+  template: _.template('<span><%= title %></span>'),
+  events: {
+    'click': 'alertThis'
+  },
+  alertThis: function (e){
+  	alert(this.model.get('title'));
+  },
+  render: function(){
+    this.$el.html(this.template(this.model.toJSON()));
+  }
+});
